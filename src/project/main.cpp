1be786d5773a06712eb0cf325dbc7e00293597c9
Austin Savoie <austin.savoie@uleth.ca>
2025-03-02 16:05:19 -0700

    added sine wave functionality

--- src/project/main.cpp
+++ src/project/main.cpp
@@ -5,7 +5,7 @@
 
 #include "Circle.hpp"
 
-const int WIDTH = 900, HEIGHT = 600;
+const int WIDTH = 1920, HEIGHT = 1080;
 const uint32_t COLOR_BLUE = 0xFF0000FF;
 const uint32_t COLOR_BLACK = 0x00000000;
 
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
 
   SDL_Surface* screen = SDL_GetWindowSurface(window);
   SDL_Rect erase_rect = (SDL_Rect){0, 0, WIDTH, HEIGHT};
-  Circle circle = Circle(200, 200, 80);
+  Circle circle = Circle(180, 200, 80);
 
   circle.fillCircle(screen, circle, COLOR_BLUE);
 
@@ -35,12 +35,57 @@ int main(int argc, char* argv[]) {
         circle.x = event.motion.x;
         circle.y = event.motion.y;
       }
+      if (event.type == SDL_KEYDOWN) {
+        switch (event.key.keysym.sym) {
+          case SDLK_UP:
+            circle.radius += 5;
+            break;
+          case SDLK_DOWN:
+            circle.radius -= 5;
+            break;
+          case SDLK_a:
+            circle.x -= 5;
+            break;
+          case SDLK_d:
+            circle.x += 5;
+            break;
+          case SDLK_w:
+            circle.y -= 5;
+            break;
+          case SDLK_s:
+            circle.y += 5;
+            break;
+          case SDLK_q:
+            for (double i = 0; i < 2 * M_PI; i += 0.01) {
+              circle.x += 0.5;
+              circle.y = 500 + 80 * sin(i);
+              SDL_FillRect(screen, &erase_rect, COLOR_BLACK);
+              circle.fillCircle(screen, circle, COLOR_BLUE);
+              SDL_UpdateWindowSurface(window);
+              if (circle.x > 1920) {
+                for (double j = 0; j < 2 * M_PI; j += 0.01) {
+                  circle.x -= 0.5;
+                  circle.y = 500 + 80 * sin(j);
+                  SDL_FillRect(screen, &erase_rect, COLOR_BLACK);
+                  circle.fillCircle(screen, circle, COLOR_BLUE);
+                  SDL_UpdateWindowSurface(window);
+                }
+                if (circle.x < 0) {
+                  break;
+                }
+              }
+            }
+            break;
+          case SDLK_ESCAPE:
+            simRunning = 0;
+            break;
+        }
+      }
     }
     SDL_FillRect(screen, &erase_rect, COLOR_BLACK);
     circle.fillCircle(screen, circle, COLOR_BLUE);
 
     SDL_UpdateWindowSurface(window);
-    SDL_Delay(10);
   }
 
   return 0;
